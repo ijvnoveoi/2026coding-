984c6e76d4dcf6294a3773724631b880e16b0974
ijvnoveoi <diegohan1008@gmail.com>
2026-03-19 16:33:26 +0800

    week04

---
+++ week04/week04-2.py
@@ -0,0 +1,9 @@
+#week04-2.py
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        N = len(gain)
+        ans = H = 0
+        for i in range(N):
+            H += gain[i]
+            ans = max(ans, H)
+        return ans
---
+++ week04/week04-5.py
@@ -0,0 +1,14 @@
+#week04-5.py
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        N = len(nums)
+        prefix = [0]
+        for i in range(N):
+            prefix.append( prefix[-1] + nums[i] )
+        #print(prefix)
+        postfix = [0] * (N+1)
+        for i in range(N-1, -1, -1):
+            postfix[i] = postfix[i+1] + nums[i]
+            if prefix[i] == postfix[i+1]:return i
+        #print(postfix)
+        return -1
