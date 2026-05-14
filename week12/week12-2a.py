b7a004620c714701bd7b7a77f6d0abe68eee853a
ijvnoveoi <diegohan1008@gmail.com>
2026-05-14 16:24:06 +0800

    week12

---
+++ week12/week12-2a.py
@@ -0,0 +1,13 @@
+#week12-2a.py
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        stack = [0]
+        visited = set()
+        visited.add(0)
+        while stack:
+            now = stack.pop()
+            for k in rooms[now]:
+                if k in visited: continue
+                stack.append(k)
+                visited.add(k)
+        return len(rooms) == len(visited)
