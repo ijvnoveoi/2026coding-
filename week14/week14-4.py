262eeeacb6f16decbfb3b8a57eecdfca97508214
ijvnoveoi <diegohan1008@gmail.com>
2026-05-28 16:30:22 +0800

    week14

---
+++ week14/week14-4.py
@@ -0,0 +1,16 @@
+#week14-4.py
+from functools import cache
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        @cache
+        def helper(i):
+            if i >= len(nums):
+                return 0
+
+            return nums[i] + max(helper(i+2), helper(i+3))
+
+        return max(helper(0), helper(1))
+
---
+++ week14/week14-5.py
@@ -0,0 +1,24 @@
+#weeek14-5.py
+from functools import cache
+
+class Solution:
+    def numTilings(self, n: int) -> int:
+        MOD = 10**9 + 7
+        @cache
+        def helper(n):
+
+            if n == 0:
+                return 1
+
+            if n == 1:
+                return 1
+
+            if n == 2:
+                return 2
+
+            ans = helper(n - 1) + helper(n - 2)
+
+            for i in range(3, n + 1):
+                ans = (ans + helper(n - i) * 2) % MOD
+            return ans
+        return helper(n)
